d21e5a722d8a04ab4d0a82a5e3df67a7d1144d94
YakubT <goldyakub@gmail.com>
2022-10-25 01:20:19 +0300

    I implemented signUp test

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/SignUpPage.java
@@ -0,0 +1,50 @@
+package com.qaprosoft.carina.demo.gui.pages;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class SignUpPage extends AbstractPage {
+
+    @FindBy(id = "uname")
+    private ExtendedWebElement nicknameTextBox;
+
+    @FindBy(xpath = "//*[@id = 'frmOpin']/fieldset/input[@id = \"email\"]")
+    private ExtendedWebElement emailTextBox;
+
+    @FindBy(xpath = "//*[@id = 'frmOpin']/input[@id = \"upass\"]")
+    private ExtendedWebElement passwordTextBox;
+
+    @FindBy(xpath = "//label[@for = \"%s\"]")
+    private ExtendedWebElement swiper;
+
+    @FindBy(xpath = "//div[@id= \"ucsubmit-f\"]/input[@id= \"nick-submit\"]")
+    private ExtendedWebElement submitButton;
+
+    public SignUpPage(WebDriver driver) {
+        super(driver);
+        setPageURL("/register.php3");
+    }
+
+    public void writeToNicknameTextBox(String s) {
+        nicknameTextBox.type(s);
+    }
+
+    public void writeToEmailTextBox(String s) {
+        emailTextBox.type(s);
+    }
+
+    public void writeToPasswordTextBox(String s) {
+        passwordTextBox.type(s);
+    }
+
+    public void confirmEverything() {
+        swiper.format("iagree1").click();
+        swiper.format("iagree2").click();
+    }
+
+    public void clickSubmitButton() {
+        submitButton.click();
+    }
+}
--- src/test/java/com/qaprosoft/carina/demo/MyWebTest.java
+++ src/test/java/com/qaprosoft/carina/demo/MyWebTest.java
@@ -3,16 +3,47 @@ package com.qaprosoft.carina.demo;
 import com.qaprosoft.carina.core.foundation.IAbstractTest;
 import com.qaprosoft.carina.demo.gui.components.enums.HeaderIconLink;
 import com.qaprosoft.carina.demo.gui.pages.HomePage;
+import com.qaprosoft.carina.demo.gui.pages.SignUpPage;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 
 public class MyWebTest implements IAbstractTest {
 
+    private static final Logger LOGGER = LogManager.getLogger(MyWebTest.class);
     @Test
     public void testSignUp() {
         // Open GSM Arena home page and verify page is opened
         HomePage homePage = new HomePage(getDriver());
         homePage.open();
         homePage.getHeaderMenu().clickHeaderIcon(HeaderIconLink.SIGN_UP);
+        SignUpPage signUpPage = new SignUpPage(getDriver());
+        signUpPage.writeToNicknameTextBox("UserT777");
+        Properties property = new Properties();
+        String email = "";
+        String password = "";
+        try (FileInputStream fis = new FileInputStream("src/main/resources/_testdata.properties")) {
+            property.load(fis);
+            email = property.getProperty("test_credentials").split("/")[0];
+            password = property.getProperty("test_credentials").substring(
+                    (property.getProperty("test_credentials").indexOf(":"))+1,
+                    (property.getProperty("test_credentials").indexOf("}")));
+        }
+        catch (IOException e){
+            LOGGER.error(e);
+        }
+        LOGGER.info(email);
+        LOGGER.info(password);
+        signUpPage.writeToEmailTextBox(email);
+        signUpPage.writeToPasswordTextBox(password);
+        signUpPage.confirmEverything();
+        signUpPage.clickSubmitButton();
     }
 }
