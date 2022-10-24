a6decb69c3b1817a2212edf6c0f27922f882f461
YakubT <goldyakub@gmail.com>
2022-10-24 21:16:07 +0300

    I created HeaderLink enum

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/components/HeaderMenu.java
@@ -0,0 +1,10 @@
+package com.qaprosoft.carina.demo.gui.components;
+
+import com.qaprosoft.carina.core.gui.AbstractUIObject;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+
+public class HeaderMenu extends AbstractUIObject {
+
+    public  HeaderMenu(WebDriver driver, SearchContext searchContext) {super(driver,searchContext);}
+}
---
+++ src/main/java/com/qaprosoft/carina/demo/gui/components/enums/HeaderLink.java
@@ -0,0 +1,29 @@
+package com.qaprosoft.carina.demo.gui.components.enums;
+
+public enum HeaderLink {
+
+    HOME("HOME"),
+
+    NEWS("NEWS"),
+
+    REVIEWS("REVIEWS"),
+
+    VIDEOS("VIDEOS"),
+
+    FEATURED("FEATURED"),
+
+    PHONE_FINDER("PHONE FINDER"),
+
+    DEALS("DEALS"),
+
+    MERCH("MERCH"),
+
+    COVERAGE("COVERAGE"),
+
+    CONTACT("CONTACT");
+    private String value;
+
+    HeaderLink(String value){
+        this.value = value;
+    }
+}
