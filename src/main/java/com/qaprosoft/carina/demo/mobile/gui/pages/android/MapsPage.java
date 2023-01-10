22818d3cb4067d3d546a78d232914d60b93c74d8
Anatolii Yakubyshyn <goldyakub@gmail.com>
2023-01-10 18:06:33 +0200

    Revert "I implemented my own Test"
    This reverts commit d48cabbbb162484bfbe05fd401110e267d41655e.

--- src/main/java/com/qaprosoft/carina/demo/mobile/gui/pages/android/MapsPage.java
+++ src/main/java/com/qaprosoft/carina/demo/mobile/gui/pages/android/MapsPage.java
@@ -1,26 +1,14 @@
 package com.qaprosoft.carina.demo.mobile.gui.pages.android;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.FindBy;
-
-import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.utils.factory.DeviceType;
 import com.qaprosoft.carina.demo.mobile.gui.pages.common.MapsPageBase;
-
+import org.openqa.selenium.WebDriver;
 
 @DeviceType(pageType = DeviceType.Type.ANDROID_PHONE, parentClass = MapsPageBase.class)
 public class MapsPage extends MapsPageBase {
 
-    @FindBy(id = "com.solvd.carinademoapplication:id/toolbar")
-    private ExtendedWebElement toolbar;
-
     public MapsPage(WebDriver driver) {
         super(driver);
-    }
-
-    @Override
-    public boolean isPageOpened() {
-        return toolbar.isElementPresent();
     }
 
 }
--- src/test/java/com/qaprosoft/carina/demo/mytests/mobile/MobileTest.java
+++ src/test/java/com/qaprosoft/carina/demo/mytests/mobile/MobileTest.java
@@ -1,15 +1,15 @@
 package com.qaprosoft.carina.demo.mytests.mobile;
 
+import com.qaprosoft.carina.demo.mobile.gui.pages.common.CarinaDescriptionPageBase;
+import com.qaprosoft.carina.demo.mobile.gui.pages.common.LoginPageBase;
+import com.qaprosoft.carina.demo.mobile.gui.pages.common.WelcomePageBase;
+import com.zebrunner.agent.core.annotation.TestLabel;
+import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.qaprosoft.carina.core.foundation.IAbstractTest;
-import com.qaprosoft.carina.demo.mobile.gui.pages.common.CarinaDescriptionPageBase;
-import com.qaprosoft.carina.demo.mobile.gui.pages.common.LoginPageBase;
-import com.qaprosoft.carina.demo.mobile.gui.pages.common.MapsPageBase;
-import com.qaprosoft.carina.demo.mobile.gui.pages.common.WelcomePageBase;
-import com.zebrunner.agent.core.annotation.TestLabel;
-import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
 import com.zebrunner.carina.utils.mobile.IMobileUtils;
 
 public class MobileTest implements IAbstractTest, IMobileUtils {
@@ -22,19 +22,12 @@ public class MobileTest implements IAbstractTest, IMobileUtils {
         Assert.assertTrue(welcomePageBase.isPageOpened(), "Welcome page isn't open");
         LoginPageBase loginPageBase = welcomePageBase.clickNextBtn();
         Assert.assertFalse(loginPageBase.isLoginBtnActive(),"Login button shouldn't be active");
-        CarinaDescriptionPageBase carinaDescriptionPage = loginPageBase.login();
+        loginPageBase.typeName("YakubT");
+        loginPageBase.typePassword(RandomStringUtils.randomAlphabetic(10));
+        loginPageBase.clickLoginBtn();
+        loginPageBase.selectMaleSex();
+        loginPageBase.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPageBase carinaDescriptionPage = loginPageBase.clickLoginBtn();
         Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
-    }
-
-    @Test(description = "testing map feature")
-    @MethodOwner(owner = "YakubT")
-    @TestLabel(name = "feature", value = {"mobile", "regression"})
-    public void testMap() {
-        WelcomePageBase welcomePage = initPage(getDriver(), WelcomePageBase.class);
-        LoginPageBase loginPage = welcomePage.clickNextBtn();
-        CarinaDescriptionPageBase carinaDescriptionPage = loginPage.login();
-        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
-        MapsPageBase mapsPage = carinaDescriptionPage.navigateToMapPage();
-        Assert.assertTrue(mapsPage.isPageOpened(), "Page didn't open");
     }
 }
