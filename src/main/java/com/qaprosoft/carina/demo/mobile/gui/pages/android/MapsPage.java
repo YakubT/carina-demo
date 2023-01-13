20b30143231be1402c9793ab5fe80b62b6181737
Anatolii Yakubyshyn <goldyakub@gmail.com>
2023-01-13 13:03:58 +0200

    implemented task3

--- src/main/java/com/qaprosoft/carina/demo/mobile/gui/pages/android/MapsPage.java
+++ src/main/java/com/qaprosoft/carina/demo/mobile/gui/pages/android/MapsPage.java
@@ -1,14 +1,60 @@
 package com.qaprosoft.carina.demo.mobile.gui.pages.android;
 
-import com.zebrunner.carina.utils.factory.DeviceType;
-import com.qaprosoft.carina.demo.mobile.gui.pages.common.MapsPageBase;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.PageOpeningStrategy;
+import com.qaprosoft.carina.demo.mobile.gui.pages.common.MapsPageBase;
+import com.zebrunner.carina.utils.factory.DeviceType;
 
 @DeviceType(pageType = DeviceType.Type.ANDROID_PHONE, parentClass = MapsPageBase.class)
 public class MapsPage extends MapsPageBase {
 
+    @FindBy (xpath = "//android.widget.TextView[contains(@text,'Map')]")
+    private ExtendedWebElement title;
+
+    @FindBy(xpath = "//android.widget.ImageView[@content-desc='Zoom in']")
+    private ExtendedWebElement zoomInBtn;
+
+    @FindBy(xpath = "//android.widget.ImageView[@content-desc='Zoom out']")
+    private ExtendedWebElement zoomOutBtn;
     public MapsPage(WebDriver driver) {
         super(driver);
+        setPageOpeningStrategy(PageOpeningStrategy.BY_ELEMENT);
+        setUiLoadedMarker(title);
+    }
+
+    @Override
+    public boolean isZoomInBtnPresent() {
+        return zoomInBtn.isElementPresent();
+    }
+
+    @Override
+    public boolean isZoomOutBtnPresent() {
+        return zoomOutBtn.isElementPresent();
+    }
+
+    @Override
+    public Point getZoomInLocation() {
+        return zoomInBtn.getLocation();
+    }
+
+    @Override
+    public Point getZoomOutLocation() {
+        return zoomOutBtn.getLocation();
+    }
+
+    @Override
+    public Dimension getZoomInSize() {
+        return zoomInBtn.getSize();
+    }
+
+    @Override
+    public Dimension getZoomOutSize() {
+        return zoomOutBtn.getSize();
     }
 
 }
--- src/main/java/com/qaprosoft/carina/demo/mobile/gui/pages/common/MapsPageBase.java
+++ src/main/java/com/qaprosoft/carina/demo/mobile/gui/pages/common/MapsPageBase.java
@@ -1,12 +1,27 @@
 package com.qaprosoft.carina.demo.mobile.gui.pages.common;
 
-import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 
-public class MapsPageBase extends AbstractPage {
+import com.qaprosoft.carina.core.gui.AbstractPage;
+
+public abstract class MapsPageBase extends AbstractPage {
 
     public MapsPageBase(WebDriver driver) {
         super(driver);
     }
+
+    public abstract boolean isZoomInBtnPresent();
+
+    public abstract boolean isZoomOutBtnPresent();
+
+    public abstract Point getZoomInLocation();
+
+    public abstract Point getZoomOutLocation();
+
+    public abstract Dimension getZoomInSize();
+
+    public abstract Dimension getZoomOutSize();
 
 }
