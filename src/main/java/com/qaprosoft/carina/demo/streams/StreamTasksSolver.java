8974895a262a96ec19ca2e558d73c48e9e1c0da6
Anatolii Yakubyshyn <goldyakub@gmail.com>
2023-03-01 18:43:30 +0200

    Created tests for my solution

---
+++ src/main/java/com/qaprosoft/carina/demo/streams/StreamTasksSolver.java
@@ -0,0 +1,18 @@
+package com.qaprosoft.carina.demo.streams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class StreamTasksSolver {
+
+    public static final Logger LOGGER = LogManager.getLogger(StreamTasksSolver.class);
+
+    public static List<String> filterNotOneSymbolStringsByFirstAndLastLetter(char c, List<String> strings) {
+        return strings.stream().filter((string) -> string.charAt(0) == c
+                && string.charAt(string.length() - 1) == c && string.length() > 1).collect(Collectors.toList());
+    }
+
+}
